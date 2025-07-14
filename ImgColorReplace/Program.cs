062b3df431187283acc1c1fe021b6b4a0a77da46
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2025-07-14 22:36:12 +0300

    searchbox & PlayerTopFlat;
    dark theme images replaced; progressbars & StatusBar fixed;
    gray & disabled colors adjusted;
    HeaderControl, TabInCaptionBar

---
+++ ImgColorReplace/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ImgColorReplace {
+
+  internal class Program {
+
+    static void Main() {
+
+      Color oldColor = Color.FromArgb(18, 8, 16); //todo: from console
+      Color newColor = Color.FromArgb(38, 28, 26); //todo: from console
+
+      while (true) {
+        Console.WriteLine("Enter base64 image to replace color or empty string to exit:");
+        var base64Image = Console.ReadLine();
+        if (string.IsNullOrEmpty(base64Image))
+          return;
+
+        byte[] imageBytes = Convert.FromBase64String(base64Image);
+        using (MemoryStream ms = new MemoryStream(imageBytes))
+        using (Bitmap bitmap = new Bitmap(ms)) {
+          for (int y = 0; y < bitmap.Height; y++) {
+            for (int x = 0; x < bitmap.Width; x++) {
+              Color pixel = bitmap.GetPixel(x, y);
+              if (pixel.R == oldColor.R && pixel.G == oldColor.G && pixel.B == oldColor.B) {
+                bitmap.SetPixel(x, y, newColor);
+              }
+            }
+          }
+
+          using (MemoryStream output = new MemoryStream()) {
+            bitmap.Save(output, ImageFormat.Png);
+            string newBase64 = Convert.ToBase64String(output.ToArray());
+            Console.WriteLine("New base64:");
+            Console.WriteLine(newBase64);
+          }
+        }
+      }
+    }
+  }
+}
---
+++ ImgColorReplace/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ImgColorReplace")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SergiyE")]
+[assembly: AssemblyProduct("ImgColorReplace")]
+[assembly: AssemblyCopyright("Copyright © Sergiy Egoshyn 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e1e64e0a-03a2-4f79-8d91-976a61d3b327")]
+
+[assembly: AssemblyVersion("1.0.*")]
